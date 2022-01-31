1867c8d26cd239fedd36cbe44265d2f0e18fc60f
artemisia000 <misiapierobon@gmail.com>
2022-01-31 16:49:31 +0100

    iniettare js con App

--- resources/js/front-office.js
+++ resources/js/front-office.js
@@ -1,1 +1,14 @@
-console.log('front');
+
+
+//importa le discendenze
+
+import Vue from 'vue';
+import App from './views/App';
+
+//inizializzazione
+
+const root = new Vue({
+    el: '#root',
+    render: h => h(App),
+});
+
