764abfbed182cf842f5f9e8c93b4d5608ba9d3c1
artemisia000 <misiapierobon@gmail.com>
2022-02-04 14:20:27 +0100

    delete

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
      <div class="container">
-         <h1>Blog Posts Index</h1>
+        <h1>Blog Posts Index</h1>
+          @if (session('deleted'))
+            <div class="alert alert-success">
+                {{ session('deleted') }}
+            </div>
+          @endif
 
          @if ($posts->isEmpty())
          <p>No found. <a href=" {{ route('admin.posts.create') }} "> Create a new post</a></p>
@@ -21,12 +26,18 @@
                     <td>{{ $post->id }}</td>
                     <td>{{ $post->title }}</td>
                     <td>
-                        <a class="btn btn-primary" href="{{ route('admin.posts.show', $post->slug) }} ">SHOW</a>
+                        <a class="btn btn-primary" href="{{ route('admin.posts.show', $post->slug) }} ">Show</a>
                     </td>
                     <td>
-                        <a class="btn btn-warning" href="{{ route('admin.posts.edit', $post->id)}}">EDIT</a>
+                        <a class="btn btn-warning" href="{{ route('admin.posts.edit', $post->id)}}">Edit</a>
                     </td>
-                    <td>DELETE</td>
+                    <td>
+                        <form action=" {{ route('admin.posts.destroy', $post->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input class="btn btn-danger" type="submit" value="Delete">                        
+                        </form>
+                    </td>
                 </tr>
                 @endforeach
             </tbody>
